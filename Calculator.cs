73f2f56c847d38c5817d13e3888c86aacffeb69a
Max <blckguard2000@gmail.com>
2023-03-19 17:53:31 +0900

    initial commit

---
+++ Calculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    internal class Calculator
+    {
+        private static int num1 { get; set; }
+        private static int num2 { get; set; }
+        private static string inputOperator { get; set; }
+
+        private static void GetInput()
+        {
+            Console.Write("Type the first operand: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Type the operator: ");
+            inputOperator = Console.ReadLine();
+            Console.Write("Type the second operand: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+        }
+
+        public static void CalculateOutput()
+        {
+            GetInput();
+
+            switch (inputOperator)
+            {
+                case "+":
+                    Console.WriteLine(num1 + num2);
+                    break;
+                case "-":
+                    Console.WriteLine(num1 - num2);
+                    break;
+                case "*":
+                    Console.WriteLine(num1 * num2);
+                    break;
+                case "/":
+                    Console.WriteLine(Convert.ToInt32(num1 / num2));
+                    break;
+            }
+        }
+    }
+}

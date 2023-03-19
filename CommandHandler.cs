bd667f8ba337770d085edf2bfa980c8636acf179
Max <blckguard2000@gmail.com>
2023-03-19 20:17:57 +0900

    fixed some bugs, this crap is still as unstable as is writing commit messages annoying

--- CommandHandler.cs
+++ CommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,50 @@ namespace Program1
 {
     internal class CommandHandler
     {
+        private string CurrentCommand { get; set; }
+        private List<int> Numbers = new() { };
+        private string Flag { get; set; }
+        public static bool QuitStatus = false;
+
+        public void SplitCommand()
+        {
+            string currentInput = Console.ReadLine();
+            string[] buffer = currentInput.Split(' ');
+            string[] numBuffer = new string[buffer.Length - 1];
+
+            CurrentCommand = buffer[0];
+            if (buffer.Last().StartsWith("-"))
+            {
+                Flag = buffer.Last();
+            }
+            
+            Array.Copy(buffer, 1, numBuffer, 0, numBuffer.Length);
+
+            foreach (string strNumber in numBuffer)
+            {
+                int number = int.Parse(strNumber);
+                Numbers.Add(number);
+            }
+        }
+
+        public int HandleCommand()
+        {
+            if (CurrentCommand == "add") 
+            {
+                return Calculator.AddNumbers(Numbers);
+            }
+            if (CurrentCommand == "quit")
+            {
+                QuitStatus = true;
+            }
+            return 0;
+        }
+
+        public static void PrintResult()
+        {
+            CommandHandler commandHandler = new CommandHandler();
+            commandHandler.SplitCommand();
+            Console.WriteLine(commandHandler.HandleCommand());
+        }
     }
 }
